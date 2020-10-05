d7f105be0a2f07b2d8a66343bac9a5ef69348f69
Jake Turchetta <14jturchetta@gmail.com>
2020-10-05 16:32:02 -0400

    SessionsLineChart created, data not rendering yet

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,32 +1,82 @@
 import React from 'react'
-import axios from 'axios'
-import { API_SECRET } from '../../../config'
+import SessionsLineChart from './SessionsLineChart.jsx'
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      from_date: '2020-08-01',
-      to_date: '2020-10-01',
-      event: 'Session Page Views'
+      fromDate: '2020-09-16',
+      toDate: '2020-09-16',
+      unit: 'hour',
+      sessionPageViews: null,
+      bodyWidth: document.body.clientWidth,
+      yMax: 400
     }
-    this.getData = this.getData.bind(this)
-
+    window.addEventListener("resize", this.resize().bind(this))
+    this.getSessionPageViews = this.getSessionPageViews.bind(this)
   }
 
   componentDidMount() {
-    this.getData()
+    this.getSessionPageViews()
   }
 
-  getData() {
-    MP.api.events("Session Page Views", { type: 'general', unit: 'hour', units: 2 }).done(results => console.log("Here", results.values()))
+  getSessionPageViews() {
+    MP.api.events("Session Page Views", {
+      type: 'general',
+      unit: this.state.unit,
+      fromDate: this.state.fromDate,
+      toDate: this.state.toDate
+    }).done(results => {
+      let data = []
+      Object.keys(results.values()['Session Page Views']).map(key => {
+        data.push({
+          date: key.substring(0, 10),
+          time: key.substring(11),
+          views: results.values()['Session Page Views'][key]
+        })
+      })
+      console.log(Math.max(...Object.values(results.values()['Session Page Views'])) * 1.1)
+      this.setState({
+        sessionPageViews: data,
+        yMax: Math.max(...Object.values(results.values()['Session Page Views'])) * 1.1
+       }, console.log(this.state))
+    })
+  }
+
+  resize() {
+    let t
+    return event => {
+      if (t !== false) {
+        clearTimeout(t)
+      }
+      t = setTimeout(() => {
+        const state = Object.assign(this.state, {
+          bodyWidth: document.body.clientWidth
+        })
+        this.setState(state)
+      }, 100)
+    }
   }
 
   render() {
     return (
       <div>
         <p> This is the react part of the application. </p>
+        {this.state.sessionPageViews ? (
+          <SessionsLineChart
+            className="SessionLineChartComponet"
+            sessionPageViews={this.state.sessionPageViews}
+            width={this.state.bodyWidth}
+            height={430}
+            xFn={d => d.time}
+            yFn={d => d.views}
+            yDomain={[0, this.state.yMax]}
+            margin={{ top: 20, left: 40, bottom: 20, right: 20 }}
+          />
+        ) : (
+          <p>Data not found.</p>
+        )}
       </div>
     )
   }
---
+++ client/src/components/SessionsLineChart.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import * as d3 from 'd3'
+import { XAxis, YAxis, YGrid, Line } from './d3/SessionsLineChart.jsx'
+
+export default class SessionsLineChart extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  updateScale(props) {
+    const data = props.sessionPageViews
+    const xScale = d3.scaleTime()
+    const yScale = d3.scaleLinear().nice()
+
+    const xDomain = d3.extent(data, d => d.time)
+    const yDomain = props.yDomain || [0, d3.max(data, d => {
+      console.log(d)
+      return props.yFn(d.views)
+    })]
+    console.log(yDomain)
+
+    xScale
+      .domain(xDomain)
+      .range([0, props.width - (props.margin.left + props.margin.right)])
+
+
+    yScale
+      .domain(yDomain)
+      .range([props.height - (props.margin.top + props.margin.bottom), 0])
+
+      return {xScale, yScale}
+  }
+
+  updatePlotSize(props){
+    const plotWidth =
+      props.width - (props.margin.left + props.margin.right)
+    const plotHeight =
+      props.height - (props.margin.top + props.margin.bottom)
+      return {plotWidth, plotHeight}
+  }
+
+  render() {
+    const {xScale, yScale} =  this.updateScale(this.props)
+
+    const {plotWidth, plotHeight} = this.updatePlotSize(this.props)
+
+    const metaData = {
+      xScale: xScale,
+      yScale: yScale,
+      plotWidth: plotWidth,
+      plotHeight: plotHeight,
+      xSlide: -xScale(this.props.xFn(this.props.sessionPageViews[0].views))
+    }
+    const plotData = {
+      plotData: this.props.sessionPageViews.map((d, i) => {
+        return {
+          id: i,
+          data: d.views,
+          x: xScale(this.props.xFn(d.views)),
+          y: yScale(this.props.yFn(d.views))
+        }
+      })
+    }
+
+    return (
+      <svg width={this.props.width} height={this.props.height}>
+        <g
+          className='axisLayer'
+          width={plotWidth}
+          height={plotHeight}
+          transform={`translate(${this.props.margin.left}, ${this.props.margin.top})`}
+        >
+          <YGrid {...metaData} />
+          <XAxis {...metaData} transform={`translate(0,${plotHeight})`} />
+          <YAxis {...metaData} />
+        </g>
+        <g
+          className='plotLayer'
+          width={plotWidth}
+          height={plotHeight}
+          transform={`translate(${this.props.margin.left}, ${this.props.margin.top})`}
+        >
+          <Line {...metaData} {...plotData} />
+        </g>
+      </svg>
+    )
+  }
+}
