79e69643617688bedc5afdee180e25181a722813
Jake Turchetta <14jturchetta@gmail.com>
2020-10-07 12:18:25 -0400

    started react sessions table

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
-import SessionsLineChart from './SessionsLineChart/SessionsLineChart.jsx'
+import styled from 'styled-components'
+import SessionsLineChart from './SessionsChart/SessionsLineChart.jsx'
 import Metrics from './Metrics/Metrics.jsx'
+import TopContentChart from './TopContent/TopContentChart.jsx'
+import { sessionsMap } from './TopContent/helper'
+
+const PieCharts = styled.div`
+
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-bottom: 50px;
+
+`
 
 export default class App extends React.Component {
 
@@ -11,17 +23,22 @@ export default class App extends React.Component {
       toDate: '2020-09-16',
       unit: 'hour',
       sessionPageViews: null,
+      sessionsMap: sessionsMap,
+      topContent: null,
       bodyWidth: document.body.clientWidth,
       yMax: 400
     }
     window.addEventListener("resize", this.resize().bind(this))
     this.getSessionPageViews = this.getSessionPageViews.bind(this)
+
   }
 
   componentDidMount() {
+    this.getTopContent()
     this.getSessionPageViews()
   }
 
+  // Session Page Views Chart
   getSessionPageViews() {
     MP.api.events("Session Page Views", {
       type: 'general',
@@ -46,6 +63,36 @@ export default class App extends React.Component {
     })
   }
 
+  // Top Content Chart
+  getTopContent() {
+    MP.api.jql(
+      function main() {
+        return Events({
+          from_date: '2020-08-01',
+          to_date:   '2020-10-01'
+        })
+        .filter(event =>  event.name == "Session Page Views")
+        .groupBy(["properties.$current_url"], mixpanel.reducer.count());
+    }).done(results => {
+      Object.keys(sessionsMap).forEach(id => {
+        results.forEach(obj => {
+          if (obj.key[0] && obj.key[0].indexOf(id) > -1) {
+            sessionsMap[id].views += obj.value
+          }
+        })
+      })
+      let topContent = []
+      for (const key in sessionsMap) {
+        topContent.push(sessionsMap[key])
+      }
+      console.log(topContent)
+      this.setState({
+        sessionsMap: sessionsMap,
+        topContent: topContent
+      })
+    })
+  }
+
   resize() {
     let t
     return event => {
@@ -64,18 +111,30 @@ export default class App extends React.Component {
   render() {
     return (
       <>
-      {this.state.sessionPageViews ? (<SessionsLineChart
-            className="SessionLineChartComponet"
-            sessionPageViews={this.state.sessionPageViews}
-            width={this.state.bodyWidth}
-            height={430}
-            xFn={d => d.time}
-            yFn={d => d.views}
-            yDomain={[0, this.state.yMax]}
-            margin={{ top: 20, left: 40, bottom: 20, right: 20 }}
-          />)
-                                  : (<p>Loading sessions chart.</p>)}
-      <Metrics />
+        {this.state.sessionPageViews ? (<SessionsLineChart
+                                          className="SessionLineChartComponet"
+                                          sessionPageViews={this.state.sessionPageViews}
+                                          width={this.state.bodyWidth}
+                                          height={430}
+                                          xFn={d => d.time}
+                                          yFn={d => d.views}
+                                          yDomain={[0, this.state.yMax]}
+                                          margin={{ top: 20, left: 40, bottom: 20, right: 20 }}
+                                        />)
+                                    : (<p>Loading sessions chart.</p>)}
+        <Metrics />
+        <PieCharts>
+          {this.state.topContent ? (<TopContentChart
+                                      data={this.state.topContent}
+                                      width={400}
+                                      height={400}
+                                      innerRadius={120}
+                                      outerRadius={200}
+                                    />)
+                                : (<p>Loading top content</p>)}
+        </PieCharts>
+
+
       </>
     )
   }
--- client/src/components/Metrics/Metrics.jsx
+++ client/src/components/Metrics/Metrics.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Promise } from 'bluebird'
+import regeneratorRuntime from 'regenerator-runtime'
 
 export default class Metrics extends React.Component {
 
@@ -8,37 +10,58 @@ export default class Metrics extends React.Component {
     this.state = {
       fromDate: "2020-08-01",
       toDate: null,
+      totalPageViews: null,
       uniqueUsers: null,
       viewsPerUser: null,
-      totalPageViews: null
     }
     this.getMetrics = this.getMetrics.bind(this)
-    this.setDates = this.setDates.bind(this)
+    this.setDate = this.setDate.bind(this)
   }
 
   // Lifecycle methods
   componentDidMount() {
-    this.setDates(this.getMetrics())
+    this.setDate(this.getMetrics())
   }
 
   // Set dates
-  setDates(cb) {
-    const today = new Date()
-    let year = today.getFullYear()
-    let date = today.getDate()
-    let month = today.getMonth() + 1
-    if (date.toString().length < 2) {
-      date = '0' + date
-    }
-    if (month.toString().length < 2) {
-      month = '0' + month
-    }
-    this.setState({
-      toDate: year + '-' + month + '-' + date
-    }, cb)
+  async setDate(cb) {
+      const today = new Date()
+      let year = today.getFullYear()
+      let date = today.getDate()
+      let month = today.getMonth() + 1
+      if (date.toString().length < 2) {
+        date = '0' + date
+      }
+      if (month.toString().length < 2) {
+        month = '0' + month
+      }
+      this.setState({
+        toDate: year + '-' + month + '-' + date
+      }, cb)
   }
 
   // HTTP Requests
+  async getViews(toDate) {
+    await new Promise((resolve, reject) => {
+      MP.api.events("Session Page Views", "Event Page Views", {
+        type: "general",
+        from_date: this.state.fromDate,
+        to_date: toDate
+      }).done(results => {
+        const views = Object.values(results.sum().values()).reduce((a, b) => a + b, 0)
+        resolve(views)
+      })
+    })
+  }
+
+  async getUsers() {
+    await new Promise((resolve, reject) => {
+      MP.api.people({
+        type: "unique"
+      }).done(results => resolve(results.values().total))
+    })
+  }
+
   getMetrics() {
     // Total session views, views / user
     MP.api.events("Session Page Views", "Event Page Views", {
@@ -80,7 +103,7 @@ export default class Metrics extends React.Component {
                                   : (null)}
         {this.state.viewsPerUser ? (
           <MetricDiv>
-            <span>Total Page Views</span>
+            <span>Views / User</span>
             <span>{this.state.viewsPerUser}</span>
           </MetricDiv>
         )
